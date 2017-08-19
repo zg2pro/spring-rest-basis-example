cf5127b2d9e8271ec2812a9424c49e587426b57a
Gregory Anne <englishblazere@free.fr>
2017-08-19 15:44:08 +0200

    put debug level to check logs interceptor

--- src/main/java/zg2pro/LibraryBoot.java
+++ src/main/java/zg2pro/LibraryBoot.java
@@ -14,16 +14,4 @@ public class LibraryBoot {
         SpringApplication.run(LibraryBoot.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-//        return args -> {
-//            System.out.println("Let's inspect the beans provided by Spring Boot:");
-//            String[] beanNames = ctx.getBeanDefinitionNames();
-//            Arrays.sort(beanNames);
-//            for (String beanName : beanNames) {
-//                System.out.println(beanName);
-//            }
-//        };
-//    }
-    
 }
