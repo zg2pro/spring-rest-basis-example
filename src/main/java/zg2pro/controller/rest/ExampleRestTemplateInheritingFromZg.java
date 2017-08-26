8c51cc4f661fe16b3d8cea62120a5c858fca20ce
Gregory Anne <englishblazere@free.fr>
2017-08-26 15:14:21 +0200

    showing how to use the errors serialization

--- src/main/java/zg2pro/controller/rest/ExampleRestTemplateInheritingFromZg.java
+++ src/main/java/zg2pro/controller/rest/ExampleRestTemplateInheritingFromZg.java
@@ -4,6 +4,7 @@ import com.github.zg2pro.spring.rest.basis.template.Zg2proRestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +18,14 @@ public class ExampleRestTemplateInheritingFromZg extends Zg2proRestTemplate {
     public ExampleRestTemplateInheritingFromZg() {
         super();
         //json reply should be application/json but we get a text/plain
-        MappingJackson2HttpMessageConverter mjhc = (MappingJackson2HttpMessageConverter) this.getMessageConverters().get(0);
-        List<MediaType> lmt = new ArrayList<>(mjhc.getSupportedMediaTypes());
-        lmt.add(MediaType.TEXT_PLAIN);
-        mjhc.setSupportedMediaTypes(lmt);
+        for (HttpMessageConverter<?> hmc : this.getMessageConverters()) {
+            if (hmc.getClass().isAssignableFrom(MappingJackson2HttpMessageConverter.class)) {
+                MappingJackson2HttpMessageConverter mjhc = (MappingJackson2HttpMessageConverter) hmc;
+                List<MediaType> lmt = new ArrayList<>(mjhc.getSupportedMediaTypes());
+                lmt.add(MediaType.TEXT_PLAIN);
+                mjhc.setSupportedMediaTypes(lmt);
+            }
+        }
     }
 
 }
